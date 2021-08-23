29ee66fd02a37583b41774ebeb82c11eab174499
lalit Kumae <lalit.k9956@gmail.com>
2021-08-23 08:52:05 +0530

    Added Localization in using asp.net core addviewlocal

---
+++ src/LocalizationAndGloblization/LocalizeCulture/LocalizeCulture/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using LocalizeCulture.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LocalizeCulture.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //private readonly IHtmlLocalizer<HomeController> _localizer;
+
+       private readonly IStringLocalizer<HomeController> _localizer;
+
+
+
+        public HomeController(ILogger<HomeController> logger,/*IHtmlLocalizer<HomeController> localizer*/ IStringLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+           // this.localizer = localizer;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            var test = _localizer["HelloWorld"];
+            ViewBag.Test = test;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(IFormCollection Skeys)
+        {
+            Person obj = new()
+            {
+                FirstName=Skeys["FirstName"],
+                LastName=Skeys["LastName"],
+                DateOfBirth=DateTime.Parse(Skeys["DateOfBirth"])
+            };
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CultureManagement(string culture, string retunUrl)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider
+                                                .MakeCookieValue(new RequestCulture(culture)),
+                                                new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
+            if(string.IsNullOrWhiteSpace(retunUrl))
+            {
+              
+
+                retunUrl ="~/";
+             }
+            
+            //ViewBag.RetunUrl = Message;
+            return LocalRedirect(retunUrl);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/LocalizationAndGloblization/LocalizeCulture/LocalizeCulture/Startup.cs
@@ -0,0 +1,107 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalizeCulture
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(opt =>
+            {
+                opt.ResourcesPath = "Resources";
+
+            });
+            //services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix,
+            //    opt =>
+            //    {
+            //        opt.ResourcesPath = "Resources";
+
+            //    })
+            //    .AddDataAnnotationsLocalization();
+            //services.Configure<RequestLocalizationOptions>(
+            //    otp =>
+            //    {
+            //        var supportedCultuers = new List<CultureInfo>
+            //        {
+            //            new CultureInfo("en"),
+            //            new CultureInfo("ar"),
+            //            new CultureInfo("hi")
+            //        };
+            //        otp.SupportedCultures = supportedCultuers;
+            //        otp.SupportedUICultures = supportedCultuers;
+            //        otp.DefaultRequestCulture = new RequestCulture("en");
+            //    });
+            services.AddControllersWithViews()
+            .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+            services.Configure<RequestLocalizationOptions>(
+                otp =>
+                {
+                    var supportedCultuers = new List<CultureInfo>
+                    {
+                        new CultureInfo("en"),
+                        new CultureInfo("hi"),
+                         new CultureInfo("ar"){DateTimeFormat={Calendar=new GregorianCalendar() } }
+                    };
+                    otp.SupportedCultures = supportedCultuers;
+                    otp.SupportedUICultures = supportedCultuers;
+                    otp.DefaultRequestCulture = new RequestCulture("en");
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseRequestLocalization(app.ApplicationServices.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+            //var supportedCultures = new[] { "en", "ar", "hi" };
+            //var locatizationsOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[2])
+            //                                                    .AddSupportedCultures(supportedCultures)
+            //                                                    .AddSupportedUICultures(supportedCultures);
+            //app.UseRequestLocalization(locatizationsOptions);
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        } 
+    }
+}
