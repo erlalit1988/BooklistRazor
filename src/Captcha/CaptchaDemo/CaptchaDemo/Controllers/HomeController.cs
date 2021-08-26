45160365fa2e2dd669315152429602cd223a931f
lalit Kumae <lalit.k9956@gmail.com>
2021-08-26 12:24:32 +0530

    Captcha

---
+++ src/Captcha/CaptchaDemo/CaptchaDemo/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using CaptchaDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace CaptchaDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.IsVerified = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string captchaCode)
+        {
+            if(captchaCode==HttpContext.Session.GetString("CaptchaCode"))
+            {
+                ViewBag.IsVerified = "Verified";
+            }
+            else
+            {
+                ViewBag.IsVerified = "Verify Failed";
+            }
+
+            return View();
+        }
+        public FileStreamResult GetImage()
+        {
+            int width = 200;
+            int height = 60;
+            var captchaCode = Captcha.GenerateCaptchaCode();
+            var result = Captcha.GenerateCaptchaImage(width, height, captchaCode);
+            HttpContext.Session.SetString("CaptchaCode", result.CaptchaCode);
+
+            Stream s = new MemoryStream(result.CaptchaByteData);
+            return new FileStreamResult(s, "image/png");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
